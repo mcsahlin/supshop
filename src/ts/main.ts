42877f04c2acdc88b4d55002e79aa0ae944dc475
Marcus C. Sahlin <mcsahlin@outlook.com>
2022-12-18 23:58:01 +0100

    Added assets, variables, mixins, html and hamburger menu styling

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,15 @@
 function createHtml(htmlTag: string): HTMLElement {
   return document.createElement(htmlTag);
 }
+
+const menuBtn = document.querySelector(".topnav__menu-btn") as HTMLDivElement;
+let menuActive: boolean = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuActive) {
+    menuBtn.classList.add("topnav__menu-btn--active");
+    menuActive = true;
+  } else {
+    menuBtn.classList.remove("topnav__menu-btn--active");
+    menuActive = false;
+  }
+});
