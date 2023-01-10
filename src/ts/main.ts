95927093ab40af66fba456463e54f22ec5750b90
Marcus C. Sahlin <mcsahlin@outlook.com>
2023-01-10 17:57:06 +0100

    debugging

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,6 @@
-import { createHtml } from './helpers';
+import { createHtml, createHtmlElementWithClassAndId } from './helpers';
 import { addSamplePack, Product } from './models/product';
-import { loadHome } from './start_page';
+import { loadHome, product_container } from './start_page';
 import { CartItem } from './models/cartitem';
 import { loadProduct } from './page_product';
 import { Page } from './models/pages';
@@ -9,13 +9,33 @@ export const page: Page = new Page();
 export const inventory: Product[] = addSamplePack();
 export const cart: CartItem[] = [];
 // Create page elements and store in list
-const mainContainer = d.getElementById('main') as HTMLDivElement;
+export const mainContainer = d.getElementById('main') as HTMLDivElement;
 d.body.appendChild(mainContainer);
-const homePage = createHtml('section', 'page page--home');
-const productPage = createHtml('section', 'page page--product');
-const cartPage = createHtml('section', 'page page--cart');
-const checkoutPage = createHtml('section', 'page page--checkout');
-const confirmationPage = createHtml('section', 'page page--confirmation');
+const homePage = createHtmlElementWithClassAndId(
+  'section',
+  'page page--home',
+  'homepage'
+);
+const productPage = createHtmlElementWithClassAndId(
+  'section',
+  'page page--product',
+  'productpage'
+);
+const cartPage = createHtmlElementWithClassAndId(
+  'section',
+  'page page--cart',
+  'cartpage'
+);
+const checkoutPage = createHtmlElementWithClassAndId(
+  'section',
+  'page page--checkout',
+  'checkoutpage'
+);
+const confirmationPage = createHtmlElementWithClassAndId(
+  'section',
+  'page page--confirmation',
+  'confirmationpage'
+);
 export const sections: HTMLElement[] = [];
 sections.push(homePage, productPage, cartPage, checkoutPage, confirmationPage);
 sections.map((s) => {
@@ -23,15 +43,23 @@ sections.map((s) => {
   s.style.display = 'none';
 });
 init();
-export function init(dest: string = 'home') {
-  sections.map((s) => {
-    s.className.substring(11) === dest ? display(s, true) : display(s, false);
-    console.log(s.className.substring(11));
-  });
-  loadPage(dest);
+export function init(dest?: string) {
+  if (dest) {
+    sections.map((s) => {
+      s.className.substring(11) === dest ? display(s, true) : display(s, false);
+      console.log(s.className.substring(11));
+    });
+    loadPage(dest);
+  }
+  // else {
+  //   sections[0].style.display = 'block';
+  //   mainContainer.appendChild(product_container);
+  //   // loadPage(page.home);
+  //   loadHome();
+  // }
 }
 function display(el: HTMLElement, activate: boolean) {
-  activate ? (el.style.display = 'flex') : (el.style.display = 'none');
+  activate ? (el.style.display = 'block') : (el.style.display = 'none');
 }
 function loadPage(dest: string, product?: Product) {
   switch (dest) {
--- src/ts/page_product.ts
+++ src/ts/page_product.ts
@@ -5,90 +5,94 @@ import {
 } from './helpers';
 import { inventory, sections } from './main';
 import { Product } from './models/product';
+
 export function loadProduct(product: Product) {
   const container = sections[1];
+  // ---------- CHECK
+  console.log(container.className);
   //#region BACK BUTTON
-  const backBtnForm = createHtml('form', 'btn-back-form');
-  const backBtnContainer = createHtml('div', 'btn-back__container');
-  const backBtnArrow = createHtml('img', 'btn--back__img');
-  const backBtn = createHtmlElementWithClassAndId(
-    'input',
-    'btn' + 'btn--back',
-    'btn-back'
-  );
-  backBtn.setAttribute('type', 'button');
-  backBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    history.back();
-  });
-  backBtnContainer.appendChild(backBtnArrow);
-  backBtnContainer.appendChild(backBtn);
-  backBtnForm.appendChild(backBtnContainer);
-  container.appendChild(backBtnContainer);
+  const back = document.createElement('button');
+  back.setAttribute('type', 'submit');
+  back.addEventListener('click', () => window.history.back());
+  // const backBtnForm = createHtml('form', 'btn-back--form');
+  // const backBtnContainer = createHtml('div', 'btn-back__container');
+  // const backBtnArrow = createHtml('img', 'btn--back__img');
+  // backBtnArrow.setAttribute('src', './assets/icons/arrow-back-white.png');
+  // const backBtn = createHtmlElementWithClassAndId(
+  //   'input',
+  //   'btn btn--back',
+  //   'btn-back'
+  // );
+  // backBtn.setAttribute('type', 'button');
+  // backBtn.addEventListener('click', (e) => {
+  //   e.preventDefault();
+  //   history.back();
+  // });
+  // backBtnContainer.appendChild(backBtnArrow);
+  // backBtnContainer.appendChild(backBtn);
+  // backBtnForm.appendChild(backBtnContainer);
+  // container.appendChild(backBtnContainer);
   //#endregion BACK BUTTON
 
   //#region PRODUCT DISPLAY
   const superContainer = createHtml('div', 'container--super');
   container.appendChild(superContainer);
   //#region IMAGE CONTAINER
-  const imageContainer = createHtml('div', 'product__img-box' + 'container');
-  const img = createHtml('img', 'prod__img') as HTMLImageElement;
+  const imageContainer = createHtml('div', 'card__imgbox container');
+  const img = createHtml('img', 'card__img') as HTMLImageElement;
   img.setAttribute('src', product.imgLink);
   img.setAttribute('alt', 'Product image');
   imageContainer.appendChild(img);
   superContainer.appendChild(imageContainer);
   //#endregion IMAGE CONTAINER
 
-  const productDetailContainer = createHtml('div', 'product__info');
+  const productDetailContainer = createHtml('div', 'card__detailbox');
   superContainer.appendChild(productDetailContainer);
-  const productDetails = createHtml('div', 'product__details' + 'container');
+  const productDetails = createHtml('div', 'card__details container');
   productDetailContainer.appendChild(productDetails);
-  const priceDetail = createHtml('span', 'details__price');
+  const priceDetail = createHtml('span', 'card__price');
   priceDetail.innerHTML = product.price + ' kr';
   productDetails.appendChild(priceDetail);
-  const labelDetail = createHtml('span', 'details__lbl');
+  const labelDetail = createHtml('span', 'card__title');
   labelDetail.innerHTML = product.label + ' || ' + inventory[0].options;
   productDetails.appendChild(labelDetail);
 
-  const optionsContainer = createHtml('div', 'product__opt-box' + 'container');
+  const optionsContainer = createHtml('div', 'opt container');
   productDetailContainer.appendChild(optionsContainer);
 
-  const productOptions = createHtml('div', 'product__opt');
+  const productOptions = createHtml('div', 'opt__flavsize');
   optionsContainer.appendChild(productOptions);
 
-  const flavorMarkupLabel = createHtml('label', 'product__flav-lbl');
-  flavorMarkupLabel.setAttribute('for', 'product__flav-sel');
+  const flavorMarkupLabel = createHtml('label', 'opt__flavsize-heading');
+  flavorMarkupLabel.setAttribute('for', 'opt__flavsize-select');
   flavorMarkupLabel.innerHTML = 'Choose flavor / size';
   productOptions.appendChild(flavorMarkupLabel);
 
   const optionsDropdown = createHtmlElementWithClassAndId(
     'select',
-    'product__flav-sel',
-    'product__flav-sel'
+    'opt__flavsize-select',
+    'opt__flavsize-select'
   );
   optionsDropdown.setAttribute('placeholder', "Click 'n pick..");
   product.options.map((opt) => {
-    let newOpt = createHtml('option', 'pill-opt') as HTMLOptionElement;
+    let newOpt = createHtml('option', 'fz__alt') as HTMLOptionElement;
     newOpt.innerHTML = opt;
     optionsDropdown.appendChild(newOpt);
   });
 
   productOptions.appendChild(optionsDropdown);
 
-  const quantitySelectionContainer = createHtml(
-    'div',
-    'product__qty-container'
-  );
+  const quantitySelectionContainer = createHtml('div', 'opt__qty');
   optionsContainer.appendChild(quantitySelectionContainer);
 
-  const quantityMarkupLabel = createHtml('label', 'product__qty-label');
+  const quantityMarkupLabel = createHtml('label', 'opt__qty-heading');
   quantityMarkupLabel.setAttribute('for', 'qty');
   quantitySelectionContainer.appendChild(quantityMarkupLabel);
   //#region QUANTITY
   //#region INPUT
   const qtyInput = document.createElement('input');
   qtyInput.id = 'qty';
-  qtyInput.className = 'product__qty-input';
+  qtyInput.className = 'opt__input';
   qtyInput.setAttribute('type', 'number');
   qtyInput.setAttribute('value', '1');
   qtyInput.addEventListener('blur', () => {
@@ -99,10 +103,7 @@ export function loadProduct(product: Product) {
   quantityMarkupLabel.appendChild(qtyInput);
   //#endregion QUANTITY INPUT
   //#region DECREASE BUTTON
-  const decreaseBtn = createHtml(
-    'button',
-    'product__qty-btn' + 'product__qty-btn--decr'
-  );
+  const decreaseBtn = createHtml('button', 'opt__qty-btn opt__qty-btn--decr');
   decreaseBtn.setAttribute('type', 'button');
   decreaseBtn.innerHTML = '-';
   decreaseBtn.addEventListener('click', () => {
@@ -113,10 +114,7 @@ export function loadProduct(product: Product) {
   quantityMarkupLabel.appendChild(decreaseBtn);
   //#endregion DECREASE BUTTON
   //#region INCREASE BUTTON
-  const increaseBtn = createHtml(
-    'button',
-    'product__qty-btn' + 'product__qty-btn--incr'
-  );
+  const increaseBtn = createHtml('button', 'opt__qty-btn opt__qty-btn--add');
   increaseBtn.setAttribute('type', 'button');
   increaseBtn.innerHTML = '+';
   increaseBtn.addEventListener('click', () => {
@@ -129,7 +127,7 @@ export function loadProduct(product: Product) {
   //#endregion QUANTITY
 
   // Buy button
-  const buyBtn = createHtml('button', 'btn' + 'product__btn-buy' + 'btn--buy');
+  const buyBtn = createHtml('button', 'btn opt__btn--buy btn--buy');
   buyBtn.innerHTML = 'Add to cart';
   buyBtn.setAttribute('type', 'button');
   quantitySelectionContainer.appendChild(buyBtn);
@@ -137,20 +135,73 @@ export function loadProduct(product: Product) {
   // FIX BEM -- Product Description
   const productDescription = createHtml(
     'article',
-    'product__description' + 'container' + 'container--description'
+    'description container container--description'
   );
   container.appendChild(productDescription);
-  const descriptionText = createHtml('p', 'specs');
+  const descriptionText = createHtml('p', 'description__content');
   productDescription.appendChild(descriptionText);
   descriptionText.innerHTML = product.description;
 
-  const productSuggestions = createHtml('div', 'promo-section');
-  container.appendChild(productSuggestions);
-  const promoHeading = createHtml('h3', 'promo__heading');
-  promoHeading.innerHTML = 'Others also bought:';
-  productSuggestions.appendChild(promoHeading);
-  const promos = createHtml('div', 'promo' + 'container');
-  productSuggestions.appendChild(promos);
+  container.appendChild(generateSuggestions());
 
-  //#region Price and label
+  // Suggestion printer
+  function printRandomObjects(
+    promoSlots: HTMLSpanElement[],
+    nums: number[],
+    promoContainer: HTMLElement
+  ): HTMLElement {
+    for (let i = 0; i < promoSlots.length; i++) {
+      for (let j = 0; j < nums.length; j++) {
+        let item: Product = inventory[j];
+        let img = createHtml('img', 'suggest__img');
+        img.setAttribute('src', item.imgLink); // set img source
+        promoSlots[i].appendChild(img);
+        let txt = createHtml('span', 'suggest__title');
+        txt.innerHTML = item.label;
+        promoSlots[i].appendChild(txt);
+        let link = createHtml('a', 'suggest__link');
+        link.appendChild(promoSlots[i]);
+        promoContainer.appendChild(link);
+      }
+    }
+    return promoContainer;
+  }
+  /* Random number generator */
+  function generateRandomInteger() {
+    const nums: number[] = [];
+    const goal: number = 4;
+    while (nums.length !== goal) {
+      let randomNumber: number = Math.floor(
+        Math.random() * inventory.length + 1
+      );
+      let cleared: boolean = false;
+      for (let i = 0; i < nums.length; i++) {
+        if (randomNumber === nums[i]) {
+          cleared = false;
+          break;
+        } else {
+          cleared = true;
+          continue;
+        }
+      }
+      if (cleared) {
+        nums.push(randomNumber);
+      }
+    }
+    return nums;
+  }
+  // Suggestion generator
+  function generateSuggestions() {
+    const promoContainer = createHtml('div', 'suggest');
+    const promo1 = createHtml('span', 'suggest__boxA');
+    const promo2 = createHtml('span', 'suggest__boxB');
+    const promo3 = createHtml('span', 'suggest__boxC');
+    const promo4 = createHtml('span', 'suggest__boxD');
+    const promoSlots = [] as HTMLSpanElement[];
+    promoSlots.push(promo1, promo2, promo3, promo4);
+    const nums: number[] = generateRandomInteger(); // Generate 4 random numbers without duplicates for product suggestions
+    printRandomObjects(promoSlots, nums, promoContainer); // Print randomized suggestions
+    //#region Price and label
+    return promoContainer;
+  }
 }
