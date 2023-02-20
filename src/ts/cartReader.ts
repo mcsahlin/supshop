18bc7b23c07c692848bed418150bcb2498ca568e
Yonatan Zewude <yonatanzewude@hotmail.com>
2023-02-20 12:30:15 +0100

    Webshop is done

--- src/ts/cartReader.ts
+++ src/ts/cartReader.ts
@@ -1,187 +1,195 @@
 import { Cart } from "./models/cart";
 import { Product } from "./models/product";
-import { createHtml, createHtmlElementWithClassAndId} from "./helpers";
+import { createHtml, createHtmlElementWithClassAndId } from "./helpers";
 
- let cartValues: Cart[] = [];
+let cartValues: Cart[] = [];
 
-function readFromLocalStorage(filename:string) {
-    let values =  localStorage.getItem(filename);
-    cartValues =  JSON.parse(values !== null? values :"") as Cart[]
-    cartValues.forEach(cartItem => {
-        populateCartData(cartItem)
-    });
+function readFromLocalStorage(filename: string) {
+  let values = localStorage.getItem(filename);
+  cartValues = JSON.parse(values !== null ? values : "") as Cart[];
+  cartValues.forEach((cartItem) => {
+    populateCartData(cartItem);
+  });
 }
 
 let totalCalculatedPrice = 0;
 const product_container = createHtml("div", "cartContent") as HTMLElement;
 
-function populateCartData(cartItem:Cart) {
-    if(cartItem.qty == 0){
-        return;
+function populateCartData(cartItem: Cart) {
+  if (cartItem.qty == 0) {
+    return;
+  }
+
+  let img = new Image(120, 120);
+  img.className = "cartImg";
+  img.setAttribute("src", cartItem.item.imgLink);
+  img.setAttribute("alt", "Product image");
+  let cartProductBox = createHtml("div", "cartBox") as HTMLElement;
+  let cartProductInfo = createHtml("div", "CartProductInfo") as HTMLElement;
+  let cartProductName = createHtml("div", "CartProductName") as HTMLElement;
+  let cartNameInfo = createHtml("div", "cartNameInfo") as HTMLBRElement;
+  let cartProductPrice = createHtmlElementWithClassAndId(
+    "div",
+    "CartProductPrice",
+    cartItem.item.id
+  ) as HTMLElement;
+  let cartProductQuantity = createHtml(
+    "input",
+    "CartProductQuantity"
+  ) as HTMLElement;
+  let item_price = Number(cartItem.item.price) * cartItem.qty + " kr";
+  cartProductQuantity.setAttribute("value", cartItem.qty.toString());
+  cartProductQuantity.setAttribute("readOnly", "true");
+  let textFormat = document.createTextNode(cartItem.item.label);
+  cartProductName.appendChild(textFormat);
+
+  let priceInText = document.createTextNode(item_price);
+
+  cartProductBox.appendChild(img);
+
+  let cartButtonInfo = createHtml("div", "infoButtonBox");
+
+  let addButton = createHtml("button", "btn_plus");
+  let addButtonText = document.createTextNode("+");
+  addButton.appendChild(addButtonText);
+
+  let buttonBox = createHtml("div", "btnBox");
+
+  let minusButton = createHtml("button", "btn_minus");
+  let minusButtonText = document.createTextNode("-");
+  minusButton.appendChild(minusButtonText);
+
+  cartProductBox.appendChild(cartButtonInfo);
+  cartNameInfo.appendChild(cartProductName);
+  cartNameInfo.appendChild(buttonBox);
+
+  cartButtonInfo.appendChild(cartNameInfo);
+  cartButtonInfo.appendChild(cartProductInfo);
+  cartProductBox.appendChild(cartButtonInfo);
+  buttonBox.appendChild(minusButton);
+  buttonBox.appendChild(cartProductQuantity);
+  buttonBox.appendChild(addButton);
+
+  totalCalculatedPrice += Number(cartItem.item.price) * cartItem.qty;
+
+  minusButton.addEventListener("click", (e) => {
+    let btnMinus = e.target as HTMLElement;
+    let btnBox = btnMinus.parentNode as HTMLElement;
+    let txtValue = btnBox.querySelector(
+      ".CartProductQuantity"
+    ) as HTMLInputElement;
+    let currentQty = handleDecrement(cartItem.item, Number(txtValue.value));
+    let cartNInfo = btnBox.parentNode as HTMLElement;
+    let infoBtnBox = cartNInfo.parentNode as HTMLElement;
+    let rootNode = infoBtnBox.parentNode as HTMLElement;
+    if (currentQty <= 0) {
+      rootNode.remove();
     }
+    cartProductQuantity.setAttribute("value", currentQty.toString());
+    let currentPrice = Number(cartItem.item.price) * currentQty;
+    updatePrice(cartItem.item.id, currentPrice);
+    totalCalculatedPrice -= Number(cartItem.item.price);
+    updateTotalPrice();
+  });
 
-    let img = new Image(120, 120);
-    img.className = "cartImg";
-    img.setAttribute("src", cartItem.item.imgLink);
-    img.setAttribute("alt", "Product image");
-    let cartProductBox = createHtml("div", "cartBox") as HTMLElement;
-    let cartProductInfo  = createHtml("div", "CartProductInfo") as HTMLElement;
-    let cartProductName = createHtml("div", "CartProductName") as HTMLElement;
-    let cartNameInfo = createHtml("div", "cartNameInfo") as HTMLBRElement;
-    let cartProductPrice = createHtmlElementWithClassAndId("div", "CartProductPrice", cartItem.item.id) as HTMLElement;
-    let cartProductQuantity = createHtml("input", "CartProductQuantity") as HTMLElement;
-    let item_price = Number(cartItem.item.price) * cartItem.qty + " kr";
-    cartProductQuantity.setAttribute("value", cartItem.qty.toString());
-    cartProductQuantity.setAttribute("readOnly", "true");
-    let textFormat = document.createTextNode(cartItem.item.label);
-    cartProductName.appendChild(textFormat);
+  addButton.addEventListener("click", (e) => {
+    let btnPlus = e.target as HTMLElement;
+    let btnBox = btnPlus.parentNode as HTMLElement;
+    let txtValue = btnBox.querySelector(
+      ".CartProductQuantity"
+    ) as HTMLInputElement;
+    let currentQty = handleIncrement(Number(txtValue.value));
+    cartProductQuantity.setAttribute("value", currentQty.toString());
+    let currentPrice = Number(cartItem.item.price) * currentQty;
+    updatePrice(cartItem.item.id, currentPrice);
+    totalCalculatedPrice += Number(cartItem.item.price);
+    updateTotalPrice();
+  });
 
-    let priceInText = document.createTextNode(item_price);
-    
-    cartProductBox.appendChild(img);
+  const deleteIcon = new Image(40, 90);
+  deleteIcon.src = "https://cdn-icons-png.flaticon.com/512/3405/3405244.png";
+  document.body.appendChild(deleteIcon);
+  deleteIcon.classList.add("removeButton");
+  handleDelete(deleteIcon, cartItem.item, cartItem.qty);
+  cartButtonInfo.appendChild(deleteIcon);
+  product_container.appendChild(cartProductBox);
 
-    let cartButtonInfo = createHtml("div", "infoButtonBox");
-
-    let addButton = createHtml("button", "btn_plus");
-    let addButtonText = document.createTextNode("+")
-    addButton.appendChild(addButtonText);
-
-    let buttonBox = createHtml("div", "btnBox");
-
-    let minusButton = createHtml("button", "btn_minus");
-    let minusButtonText = document.createTextNode("-");
-    minusButton.appendChild(minusButtonText);
-
-    cartProductBox.appendChild(cartButtonInfo);
-    cartNameInfo.appendChild(cartProductName);
-    cartNameInfo.appendChild(buttonBox);  
-
-    cartButtonInfo.appendChild(cartNameInfo);
-    cartButtonInfo.appendChild(cartProductInfo); 
-    cartProductBox.appendChild(cartButtonInfo);
-    buttonBox.appendChild(minusButton);
-    buttonBox.appendChild(cartProductQuantity);
-    buttonBox.appendChild(addButton);
-
-    totalCalculatedPrice += Number(cartItem.item.price) * cartItem.qty; 
-
-    minusButton.addEventListener("click", (e) => {
-        let btnMinus = e.target as HTMLElement
-        let btnBox = btnMinus.parentNode as HTMLElement;
-        let txtValue = btnBox.querySelector(".CartProductQuantity") as HTMLInputElement
-        let currentQty = handleDecrement(cartItem.item, Number(txtValue.value));
-        let cartNInfo = btnBox.parentNode as HTMLElement;
-        let infoBtnBox = cartNInfo.parentNode as HTMLElement;
-        let rootNode = infoBtnBox.parentNode as HTMLElement;
-        if(currentQty <= 0 ){
-            rootNode.remove();
-        }  
-        cartProductQuantity.setAttribute("value", currentQty.toString());
-        let currentPrice = Number(cartItem.item.price) * currentQty;
-        updatePrice(cartItem.item.id, currentPrice);
-        totalCalculatedPrice -= Number(cartItem.item.price);
-        updateTotalPrice();
-    });
-
-    addButton.addEventListener("click", (e) => {
-        let btnPlus = e.target as HTMLElement
-        let btnBox = btnPlus.parentNode as HTMLElement;
-        let txtValue = btnBox.querySelector(".CartProductQuantity") as HTMLInputElement
-        let currentQty = handleIncrement(Number(txtValue.value));
-        cartProductQuantity.setAttribute("value", currentQty.toString());
-        let currentPrice = Number(cartItem.item.price) * currentQty;
-        updatePrice(cartItem.item.id, currentPrice);
-        totalCalculatedPrice += Number(cartItem.item.price);
-        updateTotalPrice();
-    });
-
-    const deleteIcon = new Image(40, 90, );
-    deleteIcon.src = 'https://cdn-icons-png.flaticon.com/512/3405/3405244.png';
-    document.body.appendChild(deleteIcon);
-    deleteIcon.classList.add('removeButton');
-    handleDelete(deleteIcon, cartItem.item, cartItem.qty);
-    cartButtonInfo.appendChild(deleteIcon);
-    product_container.appendChild(cartProductBox);
-
-    cartProductPrice.appendChild(priceInText);
-    cartProductInfo.appendChild(cartProductPrice);
-
-  }
-
-  function handleDecrement(product:Product, currentQuantity:number) : number {
-        cartValues.forEach(p => {
-            if(product == p.item) {
-                p.qty = currentQuantity-1;
-                if(currentQuantity-1 == 0){
-                    const index = cartValues.indexOf(p);
-                    cartValues.splice(index, 1);    
-                    return 0;
-                }
-            }
-        });
-    
-    return --currentQuantity;
-  }
-
-  function handleIncrement(qty:number) : number {
-    return ++qty;
-  }
-
-  function handleDelete(img:HTMLElement, product:Product, quantity:number) {
-    img.addEventListener("click" , (e)=> {
-        let clickedElement = e.target as HTMLElement
-        let grandParent = clickedElement.parentNode as HTMLElement;
-        let grandGrandParent = grandParent.parentNode as HTMLElement;
-        grandGrandParent.remove()
-        cartValues.forEach(p => {
-            if(product == p.item) {
-                const index = cartValues.indexOf(p);
-                cartValues.splice(index, 1);
-            }
-        });
-        let currentTotal = 0;
-        cartValues.forEach(cartProduct =>{
-            currentTotal += Number(cartProduct.item.price) * cartProduct.qty; 
-        })
-        
-        totalCalculatedPrice = currentTotal;
-        updateTotalPrice();
-       });
-  }
-
-  function displayTotalInfo() {
-    let total_info = createHtml("div", "total");
-    let totalTitle = createHtml("div", "totalTitle");
-    total_info.appendChild(totalTitle);
-
-    let total_price_total = createHtml("div", "totalPrice");
-    total_info.appendChild(total_price_total); 
-
-    let totalTitlePrice = document.createTextNode(totalCalculatedPrice +" kr");   
-    total_price_total.appendChild(totalTitlePrice);
-
-    let totalTitleText = document.createTextNode("Total");
-    totalTitle.appendChild(totalTitleText);
-     cart.appendChild(total_info);
-
+  cartProductPrice.appendChild(priceInText);
+  cartProductInfo.appendChild(cartProductPrice);
 }
 
-
-function updatePrice(elementId:string, price:number) {
-    let totalPriceDiv = document.getElementById(elementId) as HTMLElement;
-    if(totalPriceDiv == null) {
-        return;
+function handleDecrement(product: Product, currentQuantity: number): number {
+  cartValues.forEach((p) => {
+    if (product == p.item) {
+      p.qty = currentQuantity - 1;
+      if (currentQuantity - 1 == 0) {
+        const index = cartValues.indexOf(p);
+        cartValues.splice(index, 1);
+        return 0;
+      }
     }
+  });
 
-    totalPriceDiv.innerHTML = price + " kr" 
+  return --currentQuantity;
+}
+
+function handleIncrement(qty: number): number {
+  return ++qty;
+}
+
+function handleDelete(img: HTMLElement, product: Product, quantity: number) {
+  img.addEventListener("click", (e) => {
+    let clickedElement = e.target as HTMLElement;
+    let grandParent = clickedElement.parentNode as HTMLElement;
+    let grandGrandParent = grandParent.parentNode as HTMLElement;
+    grandGrandParent.remove();
+    cartValues.forEach((p) => {
+      if (product == p.item) {
+        const index = cartValues.indexOf(p);
+        cartValues.splice(index, 1);
+      }
+    });
+    let currentTotal = 0;
+    cartValues.forEach((cartProduct) => {
+      currentTotal += Number(cartProduct.item.price) * cartProduct.qty;
+    });
+
+    totalCalculatedPrice = currentTotal;
+    updateTotalPrice();
+  });
+}
+
+function displayTotalInfo() {
+  let total_info = createHtml("div", "total");
+  let totalTitle = createHtml("div", "totalTitle");
+  total_info.appendChild(totalTitle);
+
+  let total_price_total = createHtml("div", "totalPrice");
+  total_info.appendChild(total_price_total);
+
+  let totalTitlePrice = document.createTextNode(totalCalculatedPrice + " kr");
+  total_price_total.appendChild(totalTitlePrice);
+
+  let totalTitleText = document.createTextNode("Total");
+  totalTitle.appendChild(totalTitleText);
+  cart.appendChild(total_info);
+}
+
+function updatePrice(elementId: string, price: number) {
+  let totalPriceDiv = document.getElementById(elementId) as HTMLElement;
+  if (totalPriceDiv == null) {
+    return;
+  }
+
+  totalPriceDiv.innerHTML = price + " kr";
 }
 
 function updateTotalPrice() {
-let totalPriceDiv = document.querySelector(".totalPrice") as HTMLElement;
-if(totalPriceDiv == null) {
+  let totalPriceDiv = document.querySelector(".totalPrice") as HTMLElement;
+  if (totalPriceDiv == null) {
     return;
-}
-totalPriceDiv.innerHTML = totalCalculatedPrice +" kr";
+  }
+  totalPriceDiv.innerHTML = totalCalculatedPrice + " kr";
 }
 
 const cart = document.querySelector(".cart") as HTMLElement;
@@ -191,28 +199,24 @@ displayTotalInfo();
 checkout();
 
 function checkout() {
-    let buttonCheckout = document.querySelector(".btnlink") as HTMLElement;
-    buttonCheckout.addEventListener("click", () => {
-        if(cartValues.length == 0){
-            alert("Oops cart is empty!");
-            buttonCheckout.setAttribute("href", "../index.html");   
-        }else{
-            if(validateFields()) {
-                alert("Fields are empty");
-
-            }
-        alert("Thanks for ordering our products!...")
-        buttonCheckout.setAttribute("href", "../index.html");   
-        localStorage.clear();
-        }
-    });
+  let buttonCheckout = document.querySelector(".btnlink") as HTMLElement;
+  buttonCheckout.addEventListener("click", () => {
+    if (cartValues.length == 0) {
+      alert("Oops cart is empty!");
+      buttonCheckout.setAttribute("href", "../index.html");
+    } else {
+      if (validateFields()) {
+        alert("Fields are empty");
+      }
+      alert("Thanks for ordering our products!...");
+      buttonCheckout.setAttribute("href", "../index.html");
+      localStorage.clear();
+    }
+  });
 }
 
+function validateFields(): boolean {
+  let email = document.getElementById("email") as HTMLInputElement;
 
-
-function validateFields() : boolean {
-    let email = document.getElementById("email") as HTMLInputElement;
-
-    return email.value == "" || email.value == null;
-
+  return email.value == "" || email.value == null;
 }
--- src/ts/start_page.ts
+++ src/ts/start_page.ts
@@ -2,21 +2,25 @@ import { addSamplePack, Product } from "./models/product";
 import { createHtml, createHtmlElementWithClassAndId } from "./helpers";
 import { Cart } from "./models/cart";
 const inventory: Product[] = addSamplePack();
-let cartValue:Cart[] = [];
+let cartValue: Cart[] = [];
 
 // StartPage start________________________Y
 
 const product_container = createHtml("div", "product_box_start_page");
 //counter for each products to render
 
-let counter = 0; 
+let counter = 0;
 //iterate over each product
 inventory.forEach((prodcut) => populateStaticData(prodcut));
 
 displayToCartBox("Total: 00.00", "Products: 0");
 
 function populateStaticData(product: Product) {
-  let item_box_div = createHtmlElementWithClassAndId("div", "product_box", "product_box_" + counter);
+  let item_box_div = createHtmlElementWithClassAndId(
+    "div",
+    "product_box",
+    "product_box_" + counter
+  );
   //load image from product object
   let img = new Image(120, 120);
   img.className = "img_item";
@@ -24,7 +28,11 @@ function populateStaticData(product: Product) {
   img.setAttribute("alt", "Product image");
   item_box_div.appendChild(img);
 
-  let product_info = createHtmlElementWithClassAndId("div", "product_info", "product_info_" + counter);
+  let product_info = createHtmlElementWithClassAndId(
+    "div",
+    "product_info",
+    "product_info_" + counter
+  );
 
   //get price from product and append to paragraph tag
   let item_price = product.price + " kr";
@@ -32,14 +40,22 @@ function populateStaticData(product: Product) {
   let txt = document.createTextNode(item_price);
   pg.appendChild(txt);
 
-  let add_to_cart = createHtmlElementWithClassAndId("button", "btn_add_to_cart", "btn_add_to_cart_" + counter);
+  let add_to_cart = createHtmlElementWithClassAndId(
+    "button",
+    "btn_add_to_cart",
+    "btn_add_to_cart_" + counter
+  );
   add_to_cart.innerHTML = "Add to Cart";
   let prod_name = product.label;
   let prod_link = createHtml("a", "a_prod_name");
   prod_link.innerHTML = prod_name;
-  if(prod_link !== null){
-    prod_link.addEventListener("click", () => localStorage.setItem("product_id", product.id), false);
-    prod_link.setAttribute("href", "http://localhost:1234/product.html"); 
+  if (prod_link !== null) {
+    prod_link.addEventListener(
+      "click",
+      () => localStorage.setItem("product_id", product.id),
+      false
+    );
+    prod_link.setAttribute("href", "http://localhost:1234/product.html");
   }
 
   product_info.appendChild(prod_link);
@@ -66,13 +82,17 @@ if (footer !== null) {
 document.body.appendChild(product_container);
 
 function addToCart() {
-  let btn_list = document.getElementsByClassName("btn_add_to_cart") as HTMLCollectionOf<HTMLElement>;
+  let btn_list = document.getElementsByClassName(
+    "btn_add_to_cart"
+  ) as HTMLCollectionOf<HTMLElement>;
 
   for (let i = 0; i < btn_list.length; i++) {
     btn_list[i].addEventListener("click", () => {
       let info = document.getElementById("product_info_" + i) as HTMLElement;
 
-      let to_be_removed = document.getElementById("btn_add_to_cart_" + i) as HTMLElement;
+      let to_be_removed = document.getElementById(
+        "btn_add_to_cart_" + i
+      ) as HTMLElement;
       to_be_removed.parentNode?.removeChild(to_be_removed);
       let btnContainer = createCartButtons(i);
       info.appendChild(btnContainer);
@@ -87,17 +107,33 @@ function addToCart() {
 addToCart();
 //add to Cart buttons
 function createCartButtons(id_number: number): HTMLElement {
-  let btnContainer = createHtmlElementWithClassAndId("div", "button_container","btn_container_" + id_number);
-  let btnMinus = createHtmlElementWithClassAndId("button", "btn_minus", "btn_minus_" + id_number);
+  let btnContainer = createHtmlElementWithClassAndId(
+    "div",
+    "button_container",
+    "btn_container_" + id_number
+  );
+  let btnMinus = createHtmlElementWithClassAndId(
+    "button",
+    "btn_minus",
+    "btn_minus_" + id_number
+  );
   btnMinus.innerHTML = "-";
   btnContainer.appendChild(btnMinus);
 
-  let txtField = createHtmlElementWithClassAndId("input","input_number", "input_number_" + id_number);
+  let txtField = createHtmlElementWithClassAndId(
+    "input",
+    "input_number",
+    "input_number_" + id_number
+  );
   txtField.setAttribute("type", "number");
   txtField.setAttribute("value", "1"); //initial value
   btnContainer.appendChild(txtField);
 
-  let btnAdd = createHtmlElementWithClassAndId("button", "btn_plus", "btn_plus_" + id_number);
+  let btnAdd = createHtmlElementWithClassAndId(
+    "button",
+    "btn_plus",
+    "btn_plus_" + id_number
+  );
   btnAdd.innerHTML = "+";
   btnContainer.appendChild(btnAdd);
 
@@ -109,36 +145,49 @@ function minusFromCurrentValue(currentElement: number) {
   let minus = document.getElementById("btn_minus_" + currentElement);
   if (minus !== null)
     minus.addEventListener("click", () => {
-      let txt_input = document.getElementById("input_number_" + currentElement) as HTMLInputElement;
+      let txt_input = document.getElementById(
+        "input_number_" + currentElement
+      ) as HTMLInputElement;
       let current_value: number = Number(txt_input.value) - 1;
       txt_input.value = current_value.toString();
       let product_in_cart = new Cart(inventory[currentElement], current_value);
-      for(let i = 0; i<cartValue.length; i++) {
-        if(cartValue[i].item.id === product_in_cart.item.id) {
-          if(cartValue[i].qty > 0) {
+      for (let i = 0; i < cartValue.length; i++) {
+        if (cartValue[i].item.id === product_in_cart.item.id) {
+          if (cartValue[i].qty > 0) {
             cartValue[i].qty -= 1;
-          }else {
-           cartValue = cartValue.filter(cart => cart !== cartValue[i]);
+          } else {
+            cartValue = cartValue.filter((cart) => cart !== cartValue[i]);
           }
-          let prices:number = 0;  
+          let prices: number = 0;
           let quantityCounter = 0;
-        
-          cartValue.forEach(p => {
-            prices += Number(p.item.price)*p.qty;
+
+          cartValue.forEach((p) => {
+            prices += Number(p.item.price) * p.qty;
             quantityCounter += p.qty;
           });
-          
-          updateCurrentPriceAndQuantity(prices.toString(), quantityCounter.toString());
+
+          updateCurrentPriceAndQuantity(
+            prices.toString(),
+            quantityCounter.toString()
+          );
+        }
       }
-    }
       if (current_value == 0) {
-       let to_remove = document.getElementById("btn_container_" + currentElement) as HTMLElement;
+        let to_remove = document.getElementById(
+          "btn_container_" + currentElement
+        ) as HTMLElement;
         to_remove.parentNode?.removeChild(to_remove);
-        let add_to_cart = createHtmlElementWithClassAndId("button","btn_add_to_cart", "btn_add_to_cart_" + currentElement);
+        let add_to_cart = createHtmlElementWithClassAndId(
+          "button",
+          "btn_add_to_cart",
+          "btn_add_to_cart_" + currentElement
+        );
         add_to_cart.innerHTML = "Add to Cart";
-        let product_info = document.getElementById("product_info_" + currentElement);
+        let product_info = document.getElementById(
+          "product_info_" + currentElement
+        );
         product_info?.appendChild(add_to_cart);
-       
+
         addToCart();
       }
     });
@@ -149,7 +198,9 @@ function addToCurrentValue(currentElement: number): void {
   let plus = document.getElementById("btn_plus_" + currentElement);
   if (plus !== null)
     plus.addEventListener("click", () => {
-      let btn_plus = document.getElementById("input_number_" + currentElement) as HTMLInputElement;
+      let btn_plus = document.getElementById(
+        "input_number_" + currentElement
+      ) as HTMLInputElement;
       let current_value: number = Number(btn_plus.value) + 1;
       let product_in_cart = new Cart(inventory[currentElement], current_value);
       addToCartBox(product_in_cart);
@@ -157,66 +208,66 @@ function addToCurrentValue(currentElement: number): void {
     });
 }
 
-
-function addToCartBox(product_in_cart:Cart) {
-    let product_ids_in_cart = cartValue.map(ca => ca.item.id);
-    if (cartValue.length === 0) {
-      cartValue.push(product_in_cart);
-    } else if (cartValue !== null && !(product_ids_in_cart.includes(product_in_cart.item.id))) {
-      cartValue.push(product_in_cart)    
+function addToCartBox(product_in_cart: Cart) {
+  let product_ids_in_cart = cartValue.map((ca) => ca.item.id);
+  if (cartValue.length === 0) {
+    cartValue.push(product_in_cart);
+  } else if (
+    cartValue !== null &&
+    !product_ids_in_cart.includes(product_in_cart.item.id)
+  ) {
+    cartValue.push(product_in_cart);
   } else {
-    cartValue.forEach( p=> {
-      if(p.item.id === product_in_cart.item.id) {
+    cartValue.forEach((p) => {
+      if (p.item.id === product_in_cart.item.id) {
         p.qty += 1;
       }
     });
   }
 
-  let prices:number = 0;  
+  let prices: number = 0;
   let quantityCounter = 0;
 
-  cartValue.forEach(p => {
-    prices += Number(p.item.price)*p.qty;
+  cartValue.forEach((p) => {
+    prices += Number(p.item.price) * p.qty;
     quantityCounter += p.qty;
   });
-  
+
   updateCurrentPriceAndQuantity(prices.toString(), quantityCounter.toString());
 }
 
-function displayToCartBox(caculatedPrice:string, counterText:string){
+function displayToCartBox(caculatedPrice: string, counterText: string) {
   const cartIcon = document.getElementById("cart_ic") as HTMLElement;
   const counter = createHtml("span", "counter_txt") as HTMLElement;
   counter.textContent = counterText;
   const totalPrice = createHtml("span", "total_pr");
   totalPrice.textContent = caculatedPrice + " kr";
-  const linkToStore = createHtmlElementWithClassAndId("a", "cart_box_link", "cart_box_link_id");
+  const linkToStore = createHtmlElementWithClassAndId(
+    "a",
+    "cart_box_link",
+    "cart_box_link_id"
+  );
   linkToStore.appendChild(cartIcon);
-  
+
   linkToStore.appendChild(totalPrice);
-  linkToStore.addEventListener("click", ()=> { 
-    if(cartValue.length <= 0){
+  linkToStore.addEventListener("click", () => {
+    if (cartValue.length <= 0) {
       alert("Cart is empty! Add some products");
       return;
     }
 
-    localStorage.setItem("cartValues", JSON.stringify(cartValue))
-    linkToStore.setAttribute("href", "../cart.html");     
+    localStorage.setItem("cartValues", JSON.stringify(cartValue));
+    linkToStore.setAttribute("href", "../cart.html");
   });
- 
+
   const cartContainer = document.getElementById("shopping_cart") as HTMLElement;
   cartContainer.appendChild(counter);
   cartContainer.appendChild(linkToStore);
 }
 
-function updateCurrentPriceAndQuantity(price:string, quantity:string) {
+function updateCurrentPriceAndQuantity(price: string, quantity: string) {
   let priceSpan = document.querySelector(".total_pr") as HTMLElement;
   let counter = document.querySelector(".counter_txt") as HTMLElement;
   priceSpan.textContent = "Total: " + price + " kr";
-  counter.textContent = "Products: "+ quantity;
+  counter.textContent = "Products: " + quantity;
 }
-
-
-
-
-
-
